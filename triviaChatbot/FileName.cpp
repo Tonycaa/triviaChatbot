3fa19b929df7c25644f1f1868e5e64b21626e100
Tonycaa <112794239+Tonycaa@users.noreply.github.com>
2023-04-29 11:55:30 -0700

    This is what Iv'e come up with so far. If you need me to explain anything let me know.

--- triviaChatbot/FileName.cpp
+++ triviaChatbot/FileName.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
 using namespace std;
 
-int main() {
+struct Question {
+    string question;
+    string answer;
+};
 
-	return 0;
+void readQuestions(vector<Question>& questions, string filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error: Could not open file " << filename << endl;
+        return;
+    }
+    string line;
+    int counter = 0;
+    while (getline(file, line) && counter < 13) {
+        size_t pos = line.find('@');
+        if (pos == string::npos) {
+            cout << "Error: Invalid line in file" << endl;
+            continue;
+        }
+        Question q;
+        q.question = line.substr(0, pos);
+        q.answer = line.substr(pos + 1);
+        questions.push_back(q);
+        counter++;
+    }
+    file.close();
+}
+
+void askQuestions(const vector<Question>& questions, int numQuestions) {
+    int score = 0;
+    string answer;
+    for (int i = 0; i < numQuestions; i++) {
+        int index = rand() % questions.size();
+        cout << questions[index].question << endl;
+        cin >> answer;
+        if (answer == questions[index].answer) {
+            cout << "Correct!" << endl;
+            score++;
+        }
+        else {
+            cout << "Incorrect. The correct answer is " << questions[index].answer << endl;
+        }
+    }
+    cout << "You scored " << score << " out of " << numQuestions << " questions." << endl;
+}
+
+int main() {
+    srand(time(NULL));
+    vector<Question> questions;
+    readQuestions(questions, "TriviaQuestions.txt");
+    askQuestions(questions, 13);
+    return 0;
 }
